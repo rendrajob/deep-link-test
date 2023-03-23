57b6c69a84524fb55cd95403de2cc5642c205155
rendrajob <33348259+rendrajob@users.noreply.github.com>
2023-03-23 10:37:40 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,27 +3,24 @@ import './App.css';
 
 function App() {
   const [url, setUrl] = React.useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.href(url);
+  };
   
   return (
     <div className="App">
       <header className="App-header">
         <input class="c-checkbox" type="checkbox" id="checkbox" />
         <div class="c-formContainer">
-          <form class="c-form" action="">
-            <input class="c-form__input" placeholder="E-mail" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required />
+          <form class="c-form" onSubmit={handleSubmit}>
+            <input class="c-form__input" placeholder="Enter url" type="text" required />
             <label class="c-form__buttonLabel" for="checkbox">
-              <button class="c-form__button" type="button">Send</button>
+              <button disabled={!!!url} class="c-form__button" type="button">Send</button>
             </label>
-            <label class="c-form__toggle" for="checkbox" data-title="Notify me"></label>
+            <label class="c-form__toggle" for="checkbox" data-title="Link me"></label>
           </form>
         </div>
-        <form className="form-container">
-          <div className="input-container">
-            <label className="input-label">Enter the url here</label>
-            <input className="text-input" type="text" value={url} disabled={!!!url} onChange={(e) => setUrl(e.target.value)} />
-          </div>
-          <button type="submit" className="form-button">Submit</button>
-        </form>
       </header>
     </div>
   );
