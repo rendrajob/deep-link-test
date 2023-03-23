c9b15a1837a03dc43b3434f1dc9c0e4e84419496
rendrajob <33348259+rendrajob@users.noreply.github.com>
2023-03-23 10:11:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,13 +5,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <input class="c-checkbox" type="checkbox" id="checkbox" />
-        <div className="c-formContainer">
-          <form className="c-form" action="">
-            <input className="c-form__input" placeholder="E-mail" type="text" required />
-            <label className="c-form__buttonLabel" for="checkbox">
-              <button className="c-form__button" type="button">Send</button>
+        <div class="c-formContainer">
+          <form class="c-form" action="">
+            <input class="c-form__input" placeholder="E-mail" type="text" required />
+            <label class="c-form__buttonLabel" for="checkbox">
+              <button class="c-form__button" type="button">Send</button>
             </label>
-            <label className="c-form__toggle" for="checkbox" data-title="Notify me"></label>
+            <label class="c-form__toggle" for="checkbox" data-title="Notify me"></label>
           </form>
         </div>
       </header>
