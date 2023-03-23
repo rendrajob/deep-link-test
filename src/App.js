15e1087c6a32c78dd5b4c94ce90d30dda6aa625a
rendrajob <33348259+rendrajob@users.noreply.github.com>
2023-03-23 11:22:37 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <header className="App-header">
         <input class="c-checkbox" type="checkbox" id="checkbox" />
         <div class="c-formContainer">
-          <form class="c-form" onSubmit={handleSubmit}>
-            <input value={url} onChange={(e) => setUrl(e.target.value)} class="c-form__input" placeholder="Enter url" type="text" required />
+          <form class="c-form" onsubmit={handleSubmit}>
+            <input value={url} onChange={(e) => console.log(e.target.value)} class="c-form__input" placeholder="Enter url" type="text" required />
             <label class="c-form__buttonLabel" for="checkbox">
-              <button onclick={handleSubmit} class="c-form__button" type="submit">Send</button>
+              <button onClick={handleSubmit} class="c-form__button" type="submit">Send</button>
             </label>
             <label class="c-form__toggle" for="checkbox" data-title="Link me"></label>
           </form>
