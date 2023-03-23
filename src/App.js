43862c0cd54fdcdd8b0deb12aa46b2626e72b742
rendrajob <33348259+rendrajob@users.noreply.github.com>
2023-03-23 10:27:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input class="c-checkbox" type="checkbox" id="checkbox">
+        <input class="c-checkbox" type="checkbox" id="checkbox" />
         <div class="c-formContainer">
           <form class="c-form" action="">
-            <input class="c-form__input" placeholder="E-mail" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required>
+            <input class="c-form__input" placeholder="E-mail" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required />
             <label class="c-form__buttonLabel" for="checkbox">
               <button class="c-form__button" type="button">Send</button>
             </label>
