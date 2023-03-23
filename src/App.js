2f4da2abe822b8aafd7d247e32f13fa85f72729d
rendrajob <33348259+rendrajob@users.noreply.github.com>
2023-03-23 11:29:00 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   const [url, setUrl] = React.useState('');
   const handleClick = (e) => {
     e.preventDefault();
-    console.log('The link was clicked :: ', url);
+    console.log('URL :: ', url);
+    window.location.replace(url);
   }
   
   return (
@@ -16,7 +17,7 @@ function App() {
           <div class="c-form">
             <input value={url} onChange={(e) => setUrl(e.target.value)} class="c-form__input" placeholder="Enter url" type="text" required />
             <label class="c-form__buttonLabel" for="checkbox">
-              <button onClick={handleClick} class="c-form__button">Send</button>
+              <button onClick={handleClick} disabled={!!!url} class="c-form__button">Send</button>
             </label>
             <label class="c-form__toggle" for="checkbox" data-title="Link me"></label>
           </div>
